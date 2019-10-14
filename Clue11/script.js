40190ed8afc7f9238251186c5f8b861fd0c91d4e
Anmol Jain <itsmeaj20@gmail.com>
2019-10-15 02:18:55 +0530

    Clue 11

---
+++ Clue11/script.js
@@ -0,0 +1,7 @@
+function ghapla() {
+    setTimeout(function(){
+            // $('#video').remove();
+            video = document.getElementById("video");
+            document.removeChild(video);
+    }, 5000);
+}
